1f66b2cf54af616ecde98fa8d1c22d538957ae0b
thom <thomasdimarz@gmail.com>
2025-05-12 21:32:58 +0200

    functional version

--- greedy_superstring.py
+++ greedy_superstring.py
@@ -1,5 +1,6 @@
 import itertools 
 import networkx as nx
+import random
 
 def overlap(s1, s2):
 
@@ -12,9 +13,9 @@ def overlap(s1, s2):
 
 #print(overlap('atgc','gctt'))
 
-def scs(reads:list):
-    #input : list of strings 
-    #output : shortest common superstring
+def digraph(reads:list):
+    #input : list of 
+    # #output : directed graph with w. edges  
 
     #creating Directed Graph using nx
     G = nx.DiGraph()
@@ -23,33 +24,56 @@ def scs(reads:list):
     for read1, read2 in itertools.permutations(reads, 2):
         overlap_length = overlap(read1, read2)
         if overlap_length > 0:
-            G.add_edge(read1, read2, overlap_length)
+            G.add_edge(read1, read2, weight=overlap_length)
 
-    #greedy alg. 
+    return G
 
+#g = digraph(["ATTAGACCTG", "CCTGCCGGAA", "AGACCTGCCG", "GCCGGAATAC"])
+
+def scs(reads:list):
     
-    
+    G = digraph(reads)
+
+    #chosing a random node to start with 
+    current_node = random.choice(list(G.nodes))
+
+    visited_nodes = {current_node}
+    superstring = current_node
+
+    while len(visited_nodes) < len(G.nodes):
+        next_node = None
+        max_overlap = 0
+
+        #successors in this digraph are nodes with which there is some overlap
+        for successor in G.successors(current_node):
+            if successor not in visited_nodes:
+                #getting the weight = overlap between the two nodes
+                current_overlap = G[current_node][successor]['weight']
+                if current_overlap > max_overlap:
+                    max_overlap = current_overlap
+                    next_node = successor
         
+        #if there are no nodes with overlaop i.e. no successors 
+        if next_node == None:
+            #we use the properties of a set to pick a random unvisited node
+            unvisited_nodes = set(G.nodes) - visited_nodes
+            #if unvisited_nodes is not empty
+            if unvisited_nodes:
+                next_node = random.choice(list(unvisited_nodes))
+                superstring += next_node
+            else:
+                break
+        
+        else:
+            superstring += next_node[max_overlap:]
+
+        visited_nodes.add(next_node)
+        current_node = next_node
+
+    return superstring
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(scs(["ATTAGACCTG", "CCTGCCGGAA", "AGACCTGCCG", "GCCGGAATAC"]))
 
 
 
